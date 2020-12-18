da648166783be9981ee5da2b13fc70535c0abdd2
matheus-donato <48919724+matheus-donato@users.noreply.github.com>
2020-12-18 20:44:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def create_app():
             for entry in app.db.entries.find({})
         ]
         return render_template("home.html", entries=entries_with_date)
-    return app
+    return app.run(port=5000)
